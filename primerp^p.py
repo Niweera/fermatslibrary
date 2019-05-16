2f29ce5a7182efc966fdb9ce1263d042459afd66
Nipuna Weerasekara <w.nipuna@gmail.com>
2019-05-17 00:36:51 +0530

    update primerp^p.py
    Added the command line argument for specifying the starting point.

--- primerp^p.py
+++ primerp^p.py
@@ -1,14 +1,17 @@
 import sympy 
+import sys
 
 def checkP():
-    number = 4
+    number = int(sys.argv[1])
     while (True):
         if (sympy.isprime(number)):
-            print(number)
             y = number ** number
             x = y + 2
+            print("Now checking: ",number)
             if (sympy.isprime(x)):
-                print("this is it! x: ",x," number: ",number)
+                print("=================================================\nThis is it!")
+                print("Input prime number: ",number,">>>>> Output prime number: ",x)
+                print(" ||-->> ", number," ^ ",number," + 2 = ", x)
                 break
             else:
                 number = number + 1
